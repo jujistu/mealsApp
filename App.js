74521d29cf5e8076e190108932efca71300725ca
jujistu <105232948+jujistu@users.noreply.github.com>
2023-02-27 23:42:45 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { NavigationContainer } from './node_modules/@react-navigation/native';
+
+import CategoriesScreen from './screens/CategoriesScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style='dark' />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='MealsCategories' component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
